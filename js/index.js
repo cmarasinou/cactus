55332e160aabf1c87c0083df93f0987163aa161a
Chrysostomos Marasinou <cmarasinou@g.ucla.edu>
2018-04-19 13:52:00 -0700

    Added text2lines function, new fonts, centered text on canvas

--- js/index.js
+++ js/index.js
@@ -10,6 +10,30 @@ $('form').submit(function(e){
     return false;
 });
 
+// Split the text into lines for the canvas drawing
+function text2lines(txt, ctx_canvas, font_size, font_family, max_width){
+	ctx_canvas.font=font_size+"px "+font_family;
+	words = txt.split(" ");
+	current_txt = "";
+	previous_txt = "";
+	txt_in_lines = [];//An empty array
+	n_words = words.length;
+	for (var i = 0; i < n_words; i++) {
+		current_txt += words[i] + " ";
+		if (ctx_canvas.measureText(current_txt).width >= max_width) {
+			txt_in_lines.push(previous_txt);//Add new line in array
+			current_txt = words[i];
+			previous_txt = current_txt;
+		}
+		previous_txt = current_txt;
+	}
+	if (ctx_canvas.measureText(current_txt).width >= 0){
+		txt_in_lines.push(current_txt); //Add the final line in array
+	}
+	return txt_in_lines;
+}
+
+
 $(document).ready(function(){
 
 	$('button').on('click', function(){
@@ -38,6 +62,27 @@ $(document).ready(function(){
           cactus_img.onload = function(){
             ctx.drawImage(cactus_img,canvas.width-cactus_img.width,canvas.height-cactus_img.height);
 					}
+					//ctx.font="30px Arial";
+					//ctx.fillStyle = "red";
+					//var txt="Hello World"
+					//ctx.fillText("width:" + ctx.measureText(txt).width,10,50)
+					//ctx.fillText(txt,100,100);
+					var txt="Na kateveneis mes ton psofo gia na paeis supermarket, na prp na diastaurwseis fwta pou en svista, na pieneis ws jiame j nan kleisto logw power outage epeidi fisa kai meta na perimeneis ksana mesa ston psofo gia to epomeno bus.";
+					var canvas_font_size = 30;
+					var max_width = 300;
+					var canvas_font_family = "font92942";
+					var lineheight = canvas_font_size;
+					lines = text2lines(txt, ctx, canvas_font_size, canvas_font_family, max_width);
+					//console.log(paragraph)
+					ctx.font=canvas_font_size+"px "+canvas_font_family;
+					ctx.fillStyle = "red";
+					for (var i = 0; i<lines.length; i++){
+						current_linewidth = ctx.measureText(lines[i]).width;
+						x_txt =  (max_width - current_linewidth)/2; //for centering
+						y_txt =  canvas_font_size + (i*lineheight);
+						ctx.fillText(lines[i], x_txt, y_txt);
+					}
+					console.log(lines)
 				}
 	});
 
@@ -49,14 +94,14 @@ $(document).ready(function(){
 
 
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+//!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
 
 
 
 
 
-var node = document.getElementById('mycanvas');
+//var node = document.getElementById('mycanvas');
 
 /*domtoimage.toPng(node)
     .then(function (dataUrl) {
