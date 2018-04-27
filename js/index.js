8a1d84921c30864f909938f91715f73692c22a7f
Chrysostomos Marasinou <cmarasinou@g.ucla.edu>
2018-04-27 09:29:50 -0700

    Fixed sizes on the canvas

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ $('form').submit(function(e){
 
 // Split the text into lines for the canvas drawing
 function text2lines(txt, ctx_canvas, font_size, font_family, max_width){
-	ctx_canvas.font="500 "+font_size+"px "+font_family;  //font weight 500, since canvas renders thicker fonts
 	words = txt.split(" ");
 	current_txt = "";
 	previous_txt = "";
@@ -90,39 +89,45 @@ $(document).ready(function(){
 
 		// Draw the image on canvas
 		// Set canvas size
+		var canvas_width = 2380;
+		var canvas_font_size = 175;
+		var lineheight = 1.15 * canvas_font_size;
+		padding = 50;
 		scale_factor = 5;
-		canvas.height = scale_factor*$('.story-wrap').height();
-		canvas.width = scale_factor*$('.story-wrap').width();
+		canvas.width = canvas_width;
         if (canvas.getContext) {
-          var ctx = canvas.getContext('2d');
+					var ctx = canvas.getContext('2d');
+					//Load text, split in lines, find height of it
+					var txt= $('.headline').text();
+					var max_width = canvas_width - 100; //e.g. "300px"
+					var canvas_font_family = "Gaegu";
+					ctx.font="600 " + canvas_font_size+"px "+canvas_font_family; //font weight 500, since canvas renders thicker fonts
+					lines = text2lines(txt, ctx, canvas_font_size, canvas_font_family, max_width);
+					txt_height = lineheight*lines.length;
+					//Set img size
+					img_height = 500;
+					//Set canvas height
+					canvas.height = txt_height +  img_height + 4*padding; //Where 100 is space between the two
 					// Color the canvas
 					ctx.fillStyle = $(".story").css("background-color");
 					ctx.fillRect(0, 0, canvas.width, canvas.height);
-					// Draw the cactus
+					// Draw the image
 					var cactus_img = new Image();
           cactus_img.src = "./img/cactus-mini.png";
-          // Make sure the image is loaded first otherwise nothing will draw.
-
+          	// Make sure the image is loaded first otherwise nothing will draw.
           cactus_img.onload = function(){
 						x_img = (canvas.width-scale_factor*cactus_img.width)/2; //In center
 						y_img = canvas.height-scale_factor*cactus_img.height - scale_factor*4; //At the bottom and elevate a bit
-            ctx.drawImage(cactus_img, x_img, y_img, cactus_img.width*scale_factor,  cactus_img.height*scale_factor);
+            ctx.drawImage(cactus_img, x_img, y_img, cactus_img.width*img_height/cactus_img.height,  img_height);
 					}
 					//Draw the text
-					var txt= $('.headline').text();
-					var canvas_font_size = scale_factor*30;
-					var max_width = $(".story").css("width"); //e.g. "300px"
-					max_width =  scale_factor*Number(max_width.substring(0, max_width.length - 2));
-					var canvas_font_family = "Gaegu";
-					var lineheight = 1.15 * canvas_font_size;
-					lines = text2lines(txt, ctx, canvas_font_size, canvas_font_family, max_width);
-					ctx.font="500 " + canvas_font_size+"px "+canvas_font_family; //font weight 500, since canvas renders thicker fonts
 					ctx.fillStyle = $(".story").css("color");
+					ctx.font="500 " + canvas_font_size+"px "+canvas_font_family; //font weight 500, since canvas renders thicker fonts
 					for (var i = 0; i<lines.length; i++){
 						current_linewidth = ctx.measureText(lines[i]).width;
-						x_txt =  (max_width - current_linewidth)/2; //for centering
-						y_txt =  scale_factor*10 + canvas_font_size + (i*lineheight);
-						ctx.fillText(lines[i], x_txt, y_txt);
+						x_txt =  (max_width - current_linewidth)/2 + padding; //for centering
+						y_txt =  canvas_font_size + (i*lineheight) + padding; 
+ 						ctx.fillText(lines[i], x_txt, y_txt);
 					}
 				}
 			// Post image on facebook
